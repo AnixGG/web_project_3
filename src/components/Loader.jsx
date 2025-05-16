8a8ae9a0911276512064fef91a7f7621cba074b6
AnixGG <g4zizullinn@yandex.ru>
2025-05-17 02:11:56 +0300

    delete old file

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { useAppContext } from '../context/AppContext';
-
-const Loader = () => (
-    const { loading } = useAppContext();
-    if (!loading) return null;
-    <div className="loading">Загрузка...</div>
-);
-
-export default Loader;
