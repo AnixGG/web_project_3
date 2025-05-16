1115d5576c5e7fe03cc2c2259e2ff60497c95e5c
AnixGG <g4zizullinn@yandex.ru>
2025-05-17 02:12:44 +0300

    delete old file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './components/Header';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
-import Loader from './components/Loader';
 
 const App = () => {
     return (
