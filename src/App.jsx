eb08334086c39e182657e1d7427ad10c02bdf249
AnixGG <g4zizullinn@yandex.ru>
2025-05-17 02:25:44 +0300

    add import <AppProvider> in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
+import { AppProvider } from './context/AppContext';
 
 const App = () => {
     return (
