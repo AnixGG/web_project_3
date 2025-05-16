f62392a67cfd5e199e52568d0374af2b7a5ec146
AnixGG <g4zizullinn@yandex.ru>
2025-05-17 02:16:15 +0300

    delete Loader

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ const App = () => {
         <>
             <Header />
             <main>
-                <Loader />
                 <ProductList />
                 <ProductDetails />
             </main>
